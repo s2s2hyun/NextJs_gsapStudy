23d0a603b08748e5b7cfb7aa8064b29ffdea1945
s2s2hyun <s2s2hyun0601@gmail.com>
2023-05-18 01:30:22 +0900

    main change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { makeStyles } from "@mui/styles";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { useAuth } from "@/ilb/auth";
@@ -12,6 +12,14 @@ import LoginModalPage from "@/components/Modal/LoginModal";
 import Container from "@mui/material/Container";
 import { CustomTheme } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { gsap } from "gsap";
+//@ts-ignore
+import { SplitText } from "@/util/plugin/splitText";
+
+interface EffectConfig {
+  y?: number;
+  opacity?: number;
+}
 
 interface Board {
   id: string;
@@ -34,6 +42,7 @@ const CustomText = styled("div")(({ theme }) => ({
 const CustomBackground = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
   width: "100%",
+  height: "100vh",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
@@ -55,8 +64,8 @@ const InnerContainer = styled("div")(({ theme }) => ({
   justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
-  border: "2px solid #ccc",
-  borderRadius: "5px",
+  // border: "2px solid #ccc",
+  // borderRadius: "5px",
 }));
 
 const BoradTitle = styled("h3")(({ theme }) => ({
@@ -83,19 +92,73 @@ const BoardListNumberText = styled("p")(({ theme }) => ({
 
 export default function Home() {
   // const classes = useStyles();
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const thirdText = useRef(null);
+  const fourthText = useRef(null);
+
+  useEffect(() => {
+    gsap.registerEffect({
+      name: "textEffect",
+      extendTimeline: true,
+      defaults: {
+        y: 30,
+        opacity: 0,
+      },
+      effect: (target: any, config: EffectConfig) => {
+        // console.log(target[0].dataset.index, "타겟 "); 1,2,3,4 를 찍어주고있다.
+        const index = target[0].dataset.index;
+        const tl = gsap.timeline();
+        const FirstSplit = new SplitText(target, { type: "chars" });
+        // const { y } = config.y;
+        // const { opacity } = config.opacity;
+        tl.from(FirstSplit.chars, {
+          y: config.y,
+          opacity: config.opacity,
+          stagger: 0.1,
+        }).to(FirstSplit.chars, {
+          delay: 3.2,
+          opacity: config.opacity,
+          stagger: 0.03,
+        });
+
+        return tl;
+      },
+    });
+
+    textAnimaiton();
+  }, []);
+
+  // 텍스트 애니메이션
+  const textAnimaiton = () => {
+    const animation = gsap.timeline();
+    animation
+      .add(gsap.effects.textEffect(firstText.current))
+      .add(gsap.effects.textEffect(secondText.current))
+      .add(gsap.effects.textEffect(thirdText.current))
+      .add(gsap.effects.textEffect(fourthText.current));
+
+    animation.eventCallback("onUpdate", () => {
+      //   console.log(animation.progress());
+      if (animation.progress() === 1) {
+        animation.restart();
+      }
+    });
+  };
+
   const [boardList, setBoardList] = useState<Board[]>([]);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/boards")
-      .then((res) => {
-        setBoardList(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:8080/boards")
+  //     .then((res) => {
+  //       setBoardList(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   const router = useRouter();
 
@@ -105,11 +168,53 @@ export default function Home() {
 
   return (
     <>
-      <LayHeader />
+      {/* <LayHeader /> */}
       <CustomBackground>
+        <div
+          style={{
+            width: "100%",
+            height: "100vh",
+            position: "absolute",
+            overflow: "hidden",
+          }}
+        >
+          <video
+            autoPlay
+            muted
+            loop
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <source src="/video/backVideo1.mp4" type="video/mp4" />
+          </video>
+        </div>
         <Container maxWidth="xl">
           <InnerContainer>
-            <BoradInfo>
+            {/* <h1 style={{ position: "relative", color: "white" }}>hi</h1> */}
+            <WordDiv>
+              <div className="word">
+                <div className="ex1" data-index="1" ref={firstText}>
+                  Relaxing <span>PIC</span>
+                </div>
+                <div className="ex2" data-index="2" ref={secondText}>
+                  Exciting <span>PIC</span>
+                </div>
+                <div className="ex3" data-index="3" ref={thirdText}>
+                  Kid Friendly <span>PIC</span>
+                </div>
+                <div className="ex4" data-index="4" ref={fourthText}>
+                  Romantic <span>PIC</span>
+                </div>
+              </div>
+            </WordDiv>
+            {/* <BoradInfo>
               <BoradTitle>게시글</BoradTitle>
               <div style={{ display: "table", width: "100%" }}>
                 {boardList.map((board) => (
@@ -143,12 +248,7 @@ export default function Home() {
                   </div>
                 ))}
               </div>
-            </BoradInfo>
-            <div>
-              <video autoPlay muted loop>
-                <source src="/video/backVideo1.mp4" type="video/mp4" />
-              </video>
-            </div>
+            </BoradInfo> */}
 
             {/* <CustomText style={{ paddingBottom: "5rem" }}>
               게시글 제목
@@ -178,100 +278,123 @@ export default function Home() {
   );
 }
 
-const LayHeader = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const [isClient, setIsClient] = useState(false);
-  const username = useSelector((state: RootState) => state.user.username);
-  const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
-  const [isLoginModalOpen, setIsLogginModalOpen] = useState<boolean>(false);
+const WordDiv = styled("div")`
+  position: "relative";
+  .word {
+    color: #fff;
+    font-size: 6.25rem;
+    font-weight: 700;
+    z-index: 10;
+  }
 
-  const handleLoginClick = () => {
-    setIsLogginModalOpen(!isLoginModalOpen);
-  };
+  .word > div {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+  span {
+    -webkit-text-stroke: 1px;
+    -webkit-text-stroke-color: #fff;
+    color: transparent;
+  }
+`;
 
-  // 로그아웃시 useEffect
-  // useEffect(() => {
-  //   // This will be executed whenever isLoggedIn state changes
-  //   console.log("isLoggedIn changed:", isLoggedIn);
-  // }, [isLoggedIn]);
+// const LayHeader = () => {
+//   const router = useRouter();
+//   const dispatch = useDispatch();
+//   const [isClient, setIsClient] = useState(false);
+//   const username = useSelector((state: RootState) => state.user.username);
+//   const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn);
+//   const [isLoginModalOpen, setIsLogginModalOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:8080/auth/userInfo",
-          {
-            withCredentials: true,
-          }
-        );
-        console.log(response.data.username);
-        dispatch(login({ username: response.data.username }));
-      } catch (error: any) {
-        if (error.response && error.response.status === 401) {
-          // 401 Unauthorized 에러 발생시 로그아웃 처리
-          dispatch(logout());
-        } else {
-          console.error(error);
-        }
-      }
-    };
-    fetchUser();
-  }, []);
+//   const handleLoginClick = () => {
+//     setIsLogginModalOpen(!isLoginModalOpen);
+//   };
 
-  const handleLogout = () => {
-    axios
-      .post(
-        "http://localhost:8080/auth/logout",
-        {},
-        {
-          withCredentials: true,
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res);
-        dispatch(logout());
-        window.location.reload();
-      })
-      .catch((err) => console.log(err));
-  };
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
 
-  return (
-    <>
-      <LayHeaderCustomBackground>
-        <CustomText>
-          {isClient && (
-            <>
-              {isLoggedIn ? (
-                <>
-                  username:{username}
-                  <CustomText onClick={handleLogout}>logout</CustomText>
-                </>
-              ) : (
-                <>
-                  <CustomText onClick={handleLoginClick}>login</CustomText>
-                  <CustomText onClick={() => router.push("/signup")}>
-                    signup
-                  </CustomText>
-                </>
-              )}
-            </>
-          )}
-        </CustomText>
-      </LayHeaderCustomBackground>
-      {isLoginModalOpen ? (
-        <LoginModalPage
-          setIsLogginModalOpen={setIsLogginModalOpen}
-          isLoginModalOpen={isLoginModalOpen}
-        />
-      ) : null}
-    </>
-  );
-};
+//   // 로그아웃시 useEffect
+//   // useEffect(() => {
+//   //   // This will be executed whenever isLoggedIn state changes
+//   //   console.log("isLoggedIn changed:", isLoggedIn);
+//   // }, [isLoggedIn]);
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       try {
+//         const response = await axios.get(
+//           "http://localhost:8080/auth/userInfo",
+//           {
+//             withCredentials: true,
+//           }
+//         );
+//         console.log(response.data.username);
+//         dispatch(login({ username: response.data.username }));
+//       } catch (error: any) {
+//         if (error.response && error.response.status === 401) {
+//           // 401 Unauthorized 에러 발생시 로그아웃 처리
+//           dispatch(logout());
+//         } else {
+//           console.error(error);
+//         }
+//       }
+//     };
+//     fetchUser();
+//   }, []);
+
+//   const handleLogout = () => {
+//     axios
+//       .post(
+//         "http://localhost:8080/auth/logout",
+//         {},
+//         {
+//           withCredentials: true,
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+//           },
+//         }
+//       )
+//       .then((res) => {
+//         console.log(res);
+//         dispatch(logout());
+//         window.location.reload();
+//       })
+//       .catch((err) => console.log(err));
+//   };
+
+//   return (
+//     <>
+//       <LayHeaderCustomBackground>
+//         <CustomText>
+//           {isClient && (
+//             <>
+//               {isLoggedIn ? (
+//                 <>
+//                   username:{username}
+//                   <CustomText onClick={handleLogout}>logout</CustomText>
+//                 </>
+//               ) : (
+//                 <>
+//                   <CustomText onClick={handleLoginClick}>login</CustomText>
+//                   <CustomText onClick={() => router.push("/signup")}>
+//                     signup
+//                   </CustomText>
+//                 </>
+//               )}
+//             </>
+//           )}
+//         </CustomText>
+//       </LayHeaderCustomBackground>
+//       {isLoginModalOpen ? (
+//         <LoginModalPage
+//           setIsLogginModalOpen={setIsLogginModalOpen}
+//           isLoginModalOpen={isLoginModalOpen}
+//         />
+//       ) : null}
+//     </>
+//   );
+// };
