a589420b188eec2ef992cbe764ab0044b86b5692
pre14lim <s2s2hyun0601@gmail.com>
2023-06-18 15:33:50 +0900

    yarn 이슈

--- src/components/Board/BoardCommentWrite.tsx
+++ src/components/Board/BoardCommentWrite.tsx
@@ -8,110 +8,144 @@ import { Button, Typography } from "@mui/material";
 import Checkbox from "@mui/material/Checkbox";
 
 interface CustomError extends Error {
-  message: string;
+    message: string;
 }
 
 const CustomTextField = styled(TextField)(({ theme }) => ({
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor:
-        theme.palette.mode === "light"
-          ? theme.palette.primary.main
-          : theme.palette.secondary.main,
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+            borderColor:
+                theme.palette.mode === "light"
+                    ? theme.palette.primary.main
+                    : theme.palette.secondary.main,
+        },
     },
-  },
-  width: "100%",
-  // minWidth: "300px",
+    width: "100%",
+    minWidth: "300px",
+}));
+
+const CustomEmailTextField = styled(TextField)(({ theme }) => ({
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+            borderColor:
+                theme.palette.mode === "light"
+                    ? theme.palette.primary.main
+                    : theme.palette.secondary.main,
+        },
+    },
+    width: "50%",
+    minWidth: "200px",
+}));
+
+const CustomNickNameTextField = styled(TextField)(({ theme }) => ({
+    "& .MuiOutlinedInput-root": {
+        "& fieldset": {
+            borderColor:
+                theme.palette.mode === "light"
+                    ? theme.palette.primary.main
+                    : theme.palette.secondary.main,
+        },
+    },
+    width: "50%",
+    minWidth: "200px",
 }));
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function BoardComment() {
-  // 대기
-  // const fetchBoards = async () => {
-  //   const response = await axios.post("대기");
-  //   return response.data;
-  // };
+    // 대기
+    // const fetchBoards = async () => {
+    //   const response = await axios.post("대기");
+    //   return response.data;
+    // };
 
-  // const { data, isLoading, error } = useQuery("boards", fetchBoards);
-  // //  useQuery 비동기 처리 6/4 리액트쿼리
+    // const { data, isLoading, error } = useQuery("boards", fetchBoards);
+    // //  useQuery 비동기 처리 6/4 리액트쿼리
 
-  // if (isLoading) {
-  //   return "Loading";
-  // }
+    // if (isLoading) {
+    //   return "Loading";
+    // }
 
-  // if (error) {
-  //   const errorMessage = (error as CustomError).message;
-  //   return "An Error has occurred: " + errorMessage;
-  // }
-  // if (data) {
+    // if (error) {
+    //   const errorMessage = (error as CustomError).message;
+    //   return "An Error has occurred: " + errorMessage;
+    // }
+    // if (data) {
 
-  //   console.log(data); // Example: '2 hours ago', '5 minutes ago'
-  // }
+    //   console.log(data); // Example: '2 hours ago', '5 minutes ago'
+    // }
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: "20px 0 ",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Typography variant="h1" component="h2">
-          댓글 제출
-        </Typography>
-        <Typography sx={{ fontWeight: "bold" }}>
-          이메일 주소는 공개되지 않습니다. 필수 필드는 *로 표시됩니다.
-        </Typography>
-      </Box>
-      <div>
-        <form>
-          <CustomTextField label="Board Comments" />
-          <CustomTextField label="Board Comments" />
-          <CustomTextField label="Board Comments" />
-          <label>
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <Checkbox {...label} />
-              <Typography>
-                다음 번 댓글 작성을 위해 이 브라우저에 이름, 이메일, 그리고
-                웹사이트를 저장합니다.
-              </Typography>
-            </Box>
-          </label>
-          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <Button
-              sx={{
-                fontFamily: "notokr",
-                fontSize: "20px",
-                fontWeight: 500,
-                lineHeight: "100%",
-                color: "#4a5568",
-                padding: "20px 30px",
-                border: "0",
-                borderRadius: "5px",
-                background: "#dfe5ed",
-                transition: "color 0.2s, background 0.2s",
-                "&:hover": {
-                  color: "#fff",
-                  background: "#8d99ff",
-                  transition: "color 0.2s, background 0.2s",
-                },
-              }}
-              type="submit"
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                padding: "20px 0 ",
+            }}
+        >
+            <Box
+                sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                }}
             >
-              작성
-            </Button>
-          </Box>
-        </form>
-      </div>
-    </div>
-  );
+                <Typography variant="h1" component="h2">
+                    댓글 제출
+                </Typography>
+                <Typography sx={{ fontWeight: "bold" }}>
+                    이메일 주소는 공개되지 않습니다. 필수 필드는 *로 표시됩니다.
+                </Typography>
+            </Box>
+            <div>
+                <form>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <CustomTextField label="*댓글*" />
+                        <CustomEmailTextField label="*이메일*" />
+                        <CustomNickNameTextField label="*닉네임*" />
+                    </Box>
+                    <label>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Checkbox {...label} />
+                            <Typography>
+                                다음 번 댓글 작성을 위해 이 브라우저에 이름, 이메일, 그리고
+                                웹사이트를 저장합니다.
+                            </Typography>
+                        </Box>
+                    </label>
+                    <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                        <Button
+                            sx={{
+                                fontFamily: "notokr",
+                                fontSize: "20px",
+                                fontWeight: 500,
+                                lineHeight: "100%",
+                                color: "#4a5568",
+                                padding: "20px 30px",
+                                border: "0",
+                                borderRadius: "5px",
+                                background: "#dfe5ed",
+                                transition: "color 0.2s, background 0.2s",
+                                "&:hover": {
+                                    color: "#fff",
+                                    background: "#8d99ff",
+                                    transition: "color 0.2s, background 0.2s",
+                                },
+                            }}
+                            type="submit"
+                        >
+                            작성
+                        </Button>
+                    </Box>
+                </form>
+            </div>
+        </div>
+    );
 }
--- src/pages/boardWrite/index.tsx
+++ src/pages/boardWrite/index.tsx
@@ -14,85 +14,85 @@ import { Box, Button, TextField } from "@mui/material";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 const Editor = dynamic(() => import("@/comons/Editor/Editor"), {
-  ssr: false,
-  loading: () => <p>Loading ...</p>,
+    ssr: false,
+    loading: () => <p>Loading ...</p>,
 });
 // import Editor from "@/comons/Editor/Editor";
 
 export default function BoardWrite() {
-  // 다음 우편번호 스크립트가 Nextjs 서버사이드 렌더링과 충돌하는 경우 , 이 문제를 해결하기 위해
-  // DaumPostCode 컴포넌트를 클라이언트 사이드에서만 렌더링 되도록 조건부 렌더링을 해야한다.
-  const [isClient, setIsClient] = useState(false);
+    // 다음 우편번호 스크립트가 Nextjs 서버사이드 렌더링과 충돌하는 경우 , 이 문제를 해결하기 위해
+    // DaumPostCode 컴포넌트를 클라이언트 사이드에서만 렌더링 되도록 조건부 렌더링을 해야한다.
+    const [isClient, setIsClient] = useState(false);
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  // const [isAddressOpen, setIsAddressOpen] = useState(false);
-  // const [address, setAddress] = useState("");
-  const [category, setCategory] = useState("");
-  const router = useRouter();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    // const [isAddressOpen, setIsAddressOpen] = useState(false);
+    // const [address, setAddress] = useState("");
+    const [category, setCategory] = useState("");
+    const router = useRouter();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-    try {
-      const response = await axios.post("http://localhost:8080/boards", {
-        title,
-        description,
-      });
+        try {
+            const response = await axios.post("http://localhost:8080/boards", {
+                title,
+                description,
+                category,
+            });
 
-      console.log(response.data);
+            console.log(response.data);
 
-      // 게시글 작성후 본인이 작성한 게시글 Detail 로 바로 이동
-      const postId = response.data.id;
-      router.push(`/board/${postId}`);
-      // Redirect to the main page or any other page after successful creation
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  // 다음 카카오 지도는 패키지 설치로 스크립트를 추가할수없다.
-  // 그래서 카카오 지도 관련된 필요한 컴포넌트에서만 useEffect 로 script 를 불러와주는게 가장좋다.
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
+            // 게시글 작성후 본인이 작성한 게시글 Detail 로 바로 이동
+            const postId = response.data.id;
+            router.push(`/board/${postId}`);
+            // Redirect to the main page or any other page after successful creation
+        } catch (error) {
+            console.error(error);
+        }
     };
-  }, []);
 
-  const handleChange = (event: any) => {
-    setCategory(event.target.value);
-  };
+    // 다음 카카오 지도는 패키지 설치로 스크립트를 추가할수없다.
+    // 그래서 카카오 지도 관련된 필요한 컴포넌트에서만 useEffect 로 script 를 불러와주는게 가장좋다.
 
-  // const showModal = useCallback(() => {
-  //   setIsAddressOpen(true);
-  // }, []);
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+        script.async = true;
+        document.body.appendChild(script);
 
-  // const closeModal = useCallback(() => {
-  //   setIsAddressOpen(false);
-  // }, []);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
 
-  // const handleComplete = useCallback((data: any) => {
-  //   setAddress(data.address);
-  //   // setAddressDetail(data.addressDetail); // Optional: if you want to get address detail
-  //   closeModal();
-  // }, []);
+    const handleChange = (event: any) => {
+        setCategory(event.target.value);
+    };
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+    // const showModal = useCallback(() => {
+    //   setIsAddressOpen(true);
+    // }, []);
 
-  return (
-    <Wrapper>
-      <Container maxWidth="xl">
-        <InnerContainer>
-          {/* {isAddressOpen ? (
+    // const closeModal = useCallback(() => {
+    //   setIsAddressOpen(false);
+    // }, []);
+
+    // const handleComplete = useCallback((data: any) => {
+    //   setAddress(data.address);
+    //   // setAddressDetail(data.addressDetail); // Optional: if you want to get address detail
+    //   closeModal();
+    // }, []);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return (
+        <Wrapper>
+            <Container maxWidth="xl">
+                <InnerContainer>
+                    {/* {isAddressOpen ? (
             <div
               style={{
                 width: "100%",
@@ -124,110 +124,110 @@ export default function BoardWrite() {
               </div>
             </div>
           ) : null} */}
-          <Box sx={{ margin: "1rem 0" }}>
-            <Typography variant="h1" component="h2">
-              게시글 작성
-            </Typography>
-          </Box>
-          <form
-            onSubmit={handleSubmit}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
-            }}
-          >
-            <Box>
-              <CustomSelect
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-                value={category}
-                onChange={handleChange}
-              >
-                <MenuItem value="" disabled>
-                  Placeholder
-                </MenuItem>
-                <MenuItem value={"javascript"}>javascript</MenuItem>
-                <MenuItem value={"react"}>react</MenuItem>
-                <MenuItem value={"r3f"}>r3f</MenuItem>
-              </CustomSelect>
-            </Box>
-            <h4>title</h4>
-            <TextField
-              sx={{ width: "50%", minWidth: "250px" }}
-              value={title}
-              onChange={(event) => setTitle(event.target.value)}
-            />
-            <h4>description</h4>
-            <Editor description={description} setDescription={setDescription} />
+                    <Box sx={{ margin: "1rem 0" }}>
+                        <Typography variant="h1" component="h2">
+                            게시글 작성
+                        </Typography>
+                    </Box>
+                    <form
+                        onSubmit={handleSubmit}
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <Box>
+                            <CustomSelect
+                                displayEmpty
+                                inputProps={{ "aria-label": "Without label" }}
+                                value={category}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value="" disabled>
+                                    Placeholder
+                                </MenuItem>
+                                <MenuItem value={"javascript"}>javascript</MenuItem>
+                                <MenuItem value={"react"}>react</MenuItem>
+                                <MenuItem value={"r3f"}>r3f</MenuItem>
+                            </CustomSelect>
+                        </Box>
+                        <h4>title</h4>
+                        <TextField
+                            sx={{ width: "50%", minWidth: "250px" }}
+                            value={title}
+                            onChange={(event) => setTitle(event.target.value)}
+                        />
+                        <h4>description</h4>
+                        <Editor description={description} setDescription={setDescription} />
 
-            {/* <h4>address</h4>
+                        {/* <h4>address</h4>
             <input
               value={address}
               readOnly // 주소는 검색을 통해서만 변경 가능하도록 설정
             /> */}
-            <Button
-              sx={{
-                fontFamily: "notokr",
-                fontSize: "20px",
-                fontWeight: 500,
-                lineHeight: "100%",
-                color: "#4a5568",
-                padding: "1.5rem 4rem",
-                border: "0",
-                borderRadius: "5px",
-                background: "#dfe5ed",
-                transition: "color 0.2s, background 0.2s",
-                marginTop: "3rem",
-                "&:hover": {
-                  color: "#fff",
-                  background: "#8d99ff",
-                  transition: "color 0.2s, background 0.2s",
-                },
-              }}
-              type="submit"
-            >
-              작성
-            </Button>
+                        <Button
+                            sx={{
+                                fontFamily: "notokr",
+                                fontSize: "20px",
+                                fontWeight: 500,
+                                lineHeight: "100%",
+                                color: "#4a5568",
+                                padding: "1.5rem 4rem",
+                                border: "0",
+                                borderRadius: "5px",
+                                background: "#dfe5ed",
+                                transition: "color 0.2s, background 0.2s",
+                                marginTop: "3rem",
+                                "&:hover": {
+                                    color: "#fff",
+                                    background: "#8d99ff",
+                                    transition: "color 0.2s, background 0.2s",
+                                },
+                            }}
+                            type="submit"
+                        >
+                            작성
+                        </Button>
 
-            {/* <button style={{  }} type="submit">
+                        {/* <button style={{  }} type="submit">
               전송
             </button> */}
-          </form>
-          {/* <KakaoMapPageBoard address={address} width={600} height={400} />
+                    </form>
+                    {/* <KakaoMapPageBoard address={address} width={600} height={400} />
           <button onClick={showModal}>우편번호 검색</button> */}
-        </InnerContainer>
-      </Container>
-    </Wrapper>
-  );
+                </InnerContainer>
+            </Container>
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled("section")(({ theme }) => ({
-  color: theme.palette.text.primary,
-  width: "100%",
-  // height: "100vh",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  flexDirection: "column",
-  padding: "100px 0 ",
+    color: theme.palette.text.primary,
+    width: "100%",
+    // height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    padding: "100px 0 ",
 }));
 
 const InnerContainer = styled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.background.default,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  flexDirection: "column",
+    backgroundColor: theme.palette.background.default,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
 }));
 
 const CustomSelect = styled(Select)(({ theme }) => ({
-  width: "200px", // 너비를 고정하려는 값
-  display: "flex",
-  // justifyContent: "center",
-  alignItems: "center",
-  backgroundColor: theme.palette.background.default,
-  color: theme.palette.text.primary,
-  textAlign: "center",
+    width: "200px", // 너비를 고정하려는 값
+    display: "flex",
+    // justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+    textAlign: "center",
 }));
